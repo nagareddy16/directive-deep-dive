fed7e68f09c64c046555f2ca2f4deff2af187ebb
nagareddy16 <reddynagaa@gmail.com>
2024-11-07 17:07:53 +0530

    feature/custom directives

--- src/app/learning-path/learning-path.component.ts
+++ src/app/learning-path/learning-path.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { SafeLinkDirective } from '../safe-link.directive';
 
 @Component({
   selector: 'app-learning-path',
   standalone: true,
   templateUrl: './learning-path.component.html',
-  styleUrl: './learning-path.component.css'
+  styleUrl: './learning-path.component.css',
+  imports: [SafeLinkDirective]
 })
 export class LearningPathComponent {
 
-
+  constructor(){
+    console.log("Safe link to use");
+  }
 }
